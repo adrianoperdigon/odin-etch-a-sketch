7892b8c395f02a7c0f424481b2f67d15a09e5b0b
adrianoperdigon <adrianoperdigon@gmail.com>
2024-12-22 13:51:52 -0300

    Project v1.0 Finished

---
+++ script.js
@@ -0,0 +1,50 @@
+const containerEl = document.querySelector("#container");
+
+function createGrid(squareAmount){
+
+    for (let i = 1; i < squareAmount; i++) {
+
+        //create a row to store the squares
+        const rowEl = document.createElement("div")
+        rowEl.classList.add("easRow");
+        rowEl.id = (`easRow${i}`)
+        containerEl.appendChild(rowEl)
+
+        //create the elements inside the row
+        for (let j = 1; j < squareAmount; j++) {
+            const divEl = document.createElement("div");
+            divEl.classList.add("easSquare")
+            divEl.id = (`easSquare${j}`)
+            rowEl.appendChild(divEl)
+        }
+    }
+}
+createGrid(16)
+
+    const asignListeners = ()=>{
+    const easSquares = document.querySelectorAll(".easSquare");
+    easSquares.forEach(square => {
+        square.addEventListener("mouseleave",()=>{
+            square.style.backgroundColor = "black"
+               // Incrementar la opacidad
+               let currentOpacity = parseFloat(square.style.opacity) || 0; // Si no está definido, usar 0
+               square.style.opacity = Math.min(currentOpacity + 0.1, 1); // Limitar la opacidad a 1
+    })
+
+    })}
+    asignListeners()
+
+const clearFuncionality = ()=>{
+    containerEl.replaceChildren()
+}
+
+const resizeBtn = document.querySelector("#resize")
+resizeBtn.addEventListener("click",()=>{
+    clearFuncionality()
+    let gridSize = prompt("Size?")
+    if(gridSize > 64){
+        let gridSize = prompt("Choose a number less than 100")
+    }
+    createGrid(gridSize)
+    asignListeners()
+})
